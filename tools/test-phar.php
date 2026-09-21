e918ab56d0cb007e928d623f4eed9a8c4f372e29
Vitor Mattos <vitor@php.rio>
2026-09-21 13:54:22 -0300

    test: smoke PreparedRelease finalization command in PHAR
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- tools/test-phar.php
+++ tools/test-phar.php
@@ -26,7 +26,7 @@ $run = static function (array $arguments, ?string $cwd = null, array $env = []):
     return $process;
 };
 
-foreach ([['--version'], ['list', '--raw'], ['help'], ['release:plan', '--help'], ['release:prepare', '--help']] as $arguments) {
+foreach ([['--version'], ['list', '--raw'], ['help'], ['release:plan', '--help'], ['release:prepare', '--help'], ['release:finalize', '--help']] as $arguments) {
     $run([PHP_BINARY, $phar, ...$arguments]);
 }
 
