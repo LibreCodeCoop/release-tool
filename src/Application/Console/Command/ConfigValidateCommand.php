ac68b28f43841bf7259390bfe41a31edc397b028
Vitor Mattos <vitor@php.rio>
2026-09-21 02:32:44 -0300

    feat: add strict consumer configuration v1
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/Application/Console/Command/ConfigValidateCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LibreCode\ReleaseTool\Application\Console\Command;
+
+use LibreCode\ReleaseTool\Application\Configuration\ConsumerConfigLoader;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'config:validate', description: 'Validate a release consumer configuration.')]
+final class ConfigValidateCommand extends Command
+{
+    public function __construct(private readonly ConsumerConfigLoader $loader = new ConsumerConfigLoader())
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addOption('config', null, InputOption::VALUE_REQUIRED, 'Configuration file path.', '.nextcloud-release.yml');
+        $this->addOption('json', null, InputOption::VALUE_NONE, 'Emit versioned machine-readable JSON.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $path = (string) $input->getOption('config');
+
+        try {
+            $config = $this->loader->load($path);
+        } catch (\Throwable $exception) {
+            if ($input->getOption('json')) {
+                $output->writeln((string) json_encode([
+                    'schema' => 1,
+                    'valid' => false,
+                    'error' => $exception->getMessage(),
+                ], JSON_UNESCAPED_SLASHES));
+            } else {
+                $output->writeln('<error>' . $exception->getMessage() . '</error>');
+            }
+
+            return Command::INVALID;
+        }
+
+        if ($input->getOption('json')) {
+            $output->writeln((string) json_encode([
+                'schema' => 1,
+                'valid' => true,
+                'app_id' => $config->appId,
+                'repository' => $config->repository,
+            ], JSON_UNESCAPED_SLASHES));
+
+            return Command::SUCCESS;
+        }
+
+        $output->writeln(sprintf('<info>Valid schema v%d configuration for %s.</info>', $config->schema, $config->appId));
+
+        return Command::SUCCESS;
+    }
+}
