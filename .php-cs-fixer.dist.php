6c445a85907e5cd9ba79d25e1962cffcf5721557
Vitor Mattos <vitor@php.rio>
2026-09-21 10:31:39 -0300

    fix: align bootstrap QA gates
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,6 +5,14 @@ declare(strict_types=1);
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
-$finder = Finder::create()->in([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/tools'])->append([__FILE__]);
+$finder = Finder::create()
+    ->in([__DIR__ . '/src'])
+    ->append([__FILE__]);
 
-return (new Config())->setRiskyAllowed(true)->setRules(['@PER-CS2.0' => true, '@PHP83Migration' => true, 'declare_strict_types' => true, 'ordered_imports' => true])->setFinder($finder);
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        'declare_strict_types' => true,
+        'ordered_imports' => true,
+    ])
+    ->setFinder($finder);
