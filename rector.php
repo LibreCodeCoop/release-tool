fcc635e43faf970707282fc2e7046688cd1abd36
Vitor Mattos <vitor@php.rio>
2026-09-21 10:38:52 -0300

    fix: complete release-tool CI corrections
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- rector.php
+++ rector.php
@@ -6,10 +6,4 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([__DIR__ . '/src'])
-    ->withPhpSets(php83: true)
-    ->withSkip([
-        Rector\CodeQuality\Rector\Class_\ReadonlyClassRector::class,
-        Rector\CodeQuality\Rector\Property\ReadOnlyPropertyRector::class,
-        Rector\Php83\Rector\ClassConst\AddTypeToConstRector::class,
-        Rector\Php80\Rector\Class_\StringableForToStringRector::class,
-    ]);
+    ->withPhpSets(php83: true);
