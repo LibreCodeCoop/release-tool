cc3c62e2b526a26c72fb6cd4b26d429e968f0f10
Vitor Mattos <vitor@php.rio>
2026-09-21 11:22:55 -0300

    feat: enforce release-plan context contract
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/Application/Console/Command/ReleasePlanCommand.php
+++ src/Application/Console/Command/ReleasePlanCommand.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace LibreCode\ReleaseTool\Application\Console\Command;
 
+use LibreCode\ReleaseTool\Application\Configuration\ConsumerConfigContextValidator;
 use LibreCode\ReleaseTool\Application\Configuration\ConsumerConfigLoader;
+use LibreCode\ReleaseTool\Application\Configuration\NoopConsumerConfigContextValidator;
 use LibreCode\ReleaseTool\Application\Release\PlanReleaseInput;
 use LibreCode\ReleaseTool\Application\Release\ReleasePlanning;
 use LibreCode\ReleaseTool\Domain\Release\ReleasePlan;
@@ -24,6 +26,7 @@ final class ReleasePlanCommand extends Command
     public function __construct(
         private readonly ReleasePlanning $planner,
         private readonly ConsumerConfigLoader $configLoader = new ConsumerConfigLoader(),
+        private readonly ConsumerConfigContextValidator $contextValidator = new NoopConsumerConfigContextValidator(),
     ) {
         parent::__construct();
     }
@@ -32,6 +35,7 @@ final class ReleasePlanCommand extends Command
     {
         $this
             ->addOption('config', null, InputOption::VALUE_REQUIRED, 'Consumer configuration path.', '.nextcloud-release.yml')
+            ->addOption('root', null, InputOption::VALUE_REQUIRED, 'Consumer repository root.', '.')
             ->addOption('branch', null, InputOption::VALUE_REQUIRED, 'Release branch to plan.')
             ->addOption('ref', null, InputOption::VALUE_REQUIRED, 'Immutable or resolvable planning ref.')
             ->addOption('version', null, InputOption::VALUE_REQUIRED, 'Explicit version override.')
@@ -62,6 +66,7 @@ final class ReleasePlanCommand extends Command
 
         try {
             $config = $this->configLoader->load((string) $input->getOption('config'));
+            $this->contextValidator->validate($config, (string) $input->getOption('root'));
             $plan = $this->planner->plan(
                 $config,
                 new PlanReleaseInput(
