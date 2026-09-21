405951f93273a0e7801cdc828cb538849e9765ae
Vitor Mattos <vitor@php.rio>
2026-09-21 13:52:09 -0300

    feat: wire release finalization into application
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/Application/Console/ApplicationFactory.php
+++ src/Application/Console/ApplicationFactory.php
@@ -7,8 +7,13 @@ namespace LibreCode\ReleaseTool\Application\Console;
 use LibreCode\ReleaseTool\Application\Configuration\ConsumerConfigContextValidator;
 use LibreCode\ReleaseTool\Application\Configuration\NoopConsumerConfigContextValidator;
 use LibreCode\ReleaseTool\Application\Console\Command\ConfigValidateCommand;
+use LibreCode\ReleaseTool\Application\Console\Command\MetadataInspectCommand;
+use LibreCode\ReleaseTool\Application\Console\Command\ReleaseFinalizeCommand;
 use LibreCode\ReleaseTool\Application\Console\Command\ReleasePlanCommand;
 use LibreCode\ReleaseTool\Application\Console\Command\ReleasePrepareCommand;
+use LibreCode\ReleaseTool\Application\Release\LocalReleaseMetadataInspector;
+use LibreCode\ReleaseTool\Application\Release\Port\GitRepository;
+use LibreCode\ReleaseTool\Application\Release\ReleaseFinalizer;
 use LibreCode\ReleaseTool\Application\Release\ReleasePlanning;
 use LibreCode\ReleaseTool\Application\Release\ReleasePreparationPublishing;
 use LibreCode\ReleaseTool\Application\Release\ReleasePreparer;
@@ -23,6 +28,9 @@ final class ApplicationFactory
         ?ConsumerConfigContextValidator $configValidator = null,
         ?ReleasePreparer $preparer = null,
         ?ReleasePreparationPublishing $preparationPublisher = null,
+        ?GitRepository $gitRepository = null,
+        ?LocalReleaseMetadataInspector $metadataInspector = null,
+        ?ReleaseFinalizer $finalizer = null,
     ): Application
     {
         $application = new Application('release-tool', self::version());
@@ -30,6 +38,21 @@ final class ApplicationFactory
         $application->add(new ConfigValidateCommand(
             contextValidator: $configValidator ?? new NoopConsumerConfigContextValidator(),
         ));
+
+        if ($gitRepository !== null && $metadataInspector !== null) {
+            $application->add(new MetadataInspectCommand(
+                $gitRepository,
+                $metadataInspector,
+                $configValidator ?? new NoopConsumerConfigContextValidator(),
+            ));
+        }
+
+        if ($finalizer !== null) {
+            $application->add(new ReleaseFinalizeCommand(
+                $finalizer,
+                $configValidator ?? new NoopConsumerConfigContextValidator(),
+            ));
+        }
 
         if ($planner !== null) {
             $application->add(new ReleasePlanCommand(
