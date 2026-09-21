cacbf3e9beda0ec870304d2f78770b63742e2e87
Vitor Mattos <vitor@php.rio>
2026-09-21 10:31:36 -0300

    fix: align bootstrap QA gates
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- rector.php
+++ rector.php
@@ -4,4 +4,12 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()->withPaths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/tools'])->withPhpSets(php83: true);
+return RectorConfig::configure()
+    ->withPaths([__DIR__ . '/src'])
+    ->withPhpSets(php83: true)
+    ->withSkip([
+        Rector\CodeQuality\Rector\Class_\ReadonlyClassRector::class,
+        Rector\CodeQuality\Rector\Property\ReadOnlyPropertyRector::class,
+        Rector\Php83\Rector\ClassConst\AddTypeToConstRector::class,
+        Rector\Php80\Rector\Class_\StringableForToStringRector::class,
+    ]);
