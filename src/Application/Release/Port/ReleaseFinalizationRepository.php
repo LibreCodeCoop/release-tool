d417b0dddc4131423614b2e7eb16edc7a63cb3b2
Vitor Mattos <vitor@php.rio>
2026-09-21 12:34:56 -0300

    feat: add post-merge release finalization contracts
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/Application/Release/Port/ReleaseFinalizationRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LibreCode\ReleaseTool\Application\Release\Port;
+
+use LibreCode\ReleaseTool\Application\Release\HistorySyncRequest;
+use LibreCode\ReleaseTool\Application\Release\ReadModel\FinalizedPullRequest;
+use LibreCode\ReleaseTool\Domain\Release\HistorySynchronization;
+
+interface ReleaseFinalizationRepository
+{
+    public function pullRequest(string $repository, int $number): FinalizedPullRequest;
+
+    public function branchHead(string $repository, string $branch): string;
+
+    public function publishHistorySynchronization(HistorySyncRequest $request): HistorySynchronization;
+}
